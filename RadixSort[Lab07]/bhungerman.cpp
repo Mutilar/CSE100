e7d10cddd3c341c74ba521a95f2a7d8a43298fd2
Brian <brianhungerman@gmail.com>
2018-10-09 17:29:27 -0700

    Radix Sort finished

--- RadixSort[Lab07]/bhungerman.cpp
+++ RadixSort[Lab07]/bhungerman.cpp
@@ -1,43 +1,63 @@
 /*****************************************************************
 *
 *    Author: Brian Hungerman
-*    Date: 10/6/2018
-*    Description: Using Randomized Quicksort to avoid O(N^2) worst-case
+*    Date: 10/9/2018
+*    Description: Using Radix Sort to sort "Vectors"
 *
 ******************************************************************/
 
 #include <iostream>
 #include <vector>
 #include <string>
+#include <cmath>
 
 using namespace std;
 
 int len;
 
-void quickSort(int *arr, int left, int right)
+//quick work-around for std:max not suppoorting longs
+long maximum(long x, long y)
 {
-    if ((right - left) > 1)
-    {
-        //select a value from a random index
-        int random_value = arr[(rand() % (right - left) + left)];
-        int l = left;
-        int r = right;
-        r--; //this fixed a bug...
-        while (l < r)
-        {
-            while (l < r && arr[r] > random_value) r--;
-            while (l < r + 1 && arr[l] < random_value) l++;
+    if (x > y)
+        return x;
+    return y;
+}
 
-            if (l < r) //swap (fancy way... assuming values aren't close to int.maxsize...)
-            {
-                arr[l] = arr[l] + arr[r];
-                arr[r] = arr[l] - arr[r];
-                arr[l] = arr[l] - arr[r];
-                l++;
-            }
-        }
-        quickSort(arr, left, l);
-        quickSort(arr, r, right);
+void countingSort(long arr[], long pos)
+{
+    long arr_new[len]; long current[10] = {0}; int i;
+    for (i = 0; i < len; i++)
+    {
+        current[(arr[i] / pos) % 10]++;
+    }
+    for (i = 1; i < 10; i++)
+    {
+        current[i] += current[i-1];
+    }
+    for (i = len - 1; i >= 0; i--)
+    {
+        arr_new[current[(arr[i] / pos) % 10] - 1] = arr[i];
+        current[(arr[i] / pos) % 10]--;
+    }
+    for (i = 0; i < len; i++)
+    {
+        arr[i] = arr_new[i];
+    }
+}
+
+void radixSort(long arr[])
+{
+    long max_val = -1000000;
+    long pos = 1;
+    long pass = 1;
+    for (int i = 0; i < len; i++)
+    {
+        max_val = maximum(max_val, arr[i]);
+    }
+    while (max_val / pos > 0)
+    {
+        countingSort(arr, pos);
+        pos *= 10; pass++;
     }
 }
 
@@ -45,17 +65,34 @@ int main()
 {
     /*First Input: Length of Array*/
     cin >> len;
-    int values[len];
+    long values[len];
     /*Take in Array Values*/
     for (int i = 0; i < len; i++)
     {
-        cin >> values[i];
+        //ghetto form-into-one-number
+        long new_val = 0;
+        for (int j = 0; j < 10; j++)
+        {
+            int input;
+            cin >> input;
+            new_val += input * (long)pow(10, 9 - j);
+        }
+        values[i] = new_val;
     }
     /*Sorty Sort Sort*/
-    quickSort(values, 0, len);
+    radixSort(values);
     for (int i = 0; i < len; i++)
     {
-        cout << values[i] << ";";
+        //Ghetto break-into-individual-numbers
+        long new_val = values[i];
+        long expon = 1000000000;
+        while (expon > 0)
+        {
+            cout << (new_val - (new_val % expon)) / expon << ";";
+            new_val -= (new_val - (new_val % expon));
+            expon /= 10;
+        }
+        cout << "\n";
     }
     return 0;
 }
